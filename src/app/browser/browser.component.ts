2b95a6704aa878d69888ada39ea0185e72aecbdc
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-11-28 16:14:08 +0100

    Create a component used to perform fuzzy search by card name

---
+++ src/app/browser/browser.component.ts
@@ -0,0 +1,37 @@
+import {Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BehaviorSubject, debounceTime, distinctUntilChanged, Observable, tap} from "rxjs";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {ScryfallService} from "../services/scryfall.service";
+import {Card} from "../models/card";
+import {CardComponent} from "../card/card.component";
+
+@Component({
+  selector: 'app-browser',
+  standalone: true,
+    imports: [CommonModule, CardComponent],
+  templateUrl: './browser.component.html',
+  styleUrls: ['./browser.component.scss']
+})
+export class BrowserComponent {
+  private scryfall = inject(ScryfallService);
+
+  private inputValue = new BehaviorSubject<string | undefined>(undefined);
+  private inputValue$ = this.inputValue.asObservable()
+      .pipe(
+          takeUntilDestroyed(),
+          debounceTime(1000),
+          distinctUntilChanged(),
+          tap(value => {
+                if (value) {
+                  this.browsedCards$ = this.scryfall.getCardsForAutocomplete(value);
+                }
+          })
+      ).subscribe();
+
+  browsedCards$: Observable<Card[]> | undefined;
+
+  onInputChange(input: string) {
+      this.inputValue.next(input);
+  }
+}
