f21c5cf8dcdbb45ea7d07fa0ffd37a5febe81434
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 17:33:38 +0100

    Replace functions with card class methods, add a selection indicator

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {Card} from "../models/card";
-
-enum CardFaces {
-  Front,
-  Back
-}
 
 @Component({
   selector: 'app-card',
@@ -17,25 +12,10 @@ enum CardFaces {
 export class CardComponent {
   isFlipped = false;
   @Input({ required: true }) card!: Card;
+  @Input({ required: true }) isSelected!: boolean;
+  @Output('cardSelected') faceClicked = new EventEmitter<void>();
 
-  getCardImageURI(face: CardFaces): string {
-    const card = this.card;
-
-    if (this.isTwoSided()) return card.card_faces![face.valueOf()].image_uris!.normal;
-
-    if (card.image_uris) return card.image_uris.normal;
-
-    return '../assets/images/mtg_card_back.jpeg';
-  }
-
-  isTwoSided(): boolean {
-    const card = this.card;
-    return !!(card.card_faces && card.card_faces[0].image_uris != null);
-  }
-
-  flip() {
+  flip(event: MouseEvent) {
     this.isFlipped = !this.isFlipped;
   }
-
-  protected readonly CardFaces = CardFaces;
 }
