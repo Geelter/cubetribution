31c62c57f7739b52d03b878b3428e6e1a0aab26d
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 19:56:09 +0100

    Add route configuration for card browser and collection grid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {CollectionGridComponent} from "./collection-grid/collection-grid.component";
+import {BrowserComponent} from "./browser/browser.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'collections',
+    component: CollectionGridComponent,
+  },
+  {
+    path: 'browse',
+    component: BrowserComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'collections'
+  }
+];
