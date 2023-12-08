9f727d12f4a9567a99be7933a6200fbd5974a4f6
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 18:23:39 +0100

    Create a wrapper for card grid and table used to switch between layouts

---
+++ src/app/card-list/card-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, EventEmitter, HostBinding, Input, Output} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CardGridComponent} from "../card-grid/card-grid.component";
+import {CardTableComponent} from "../card-table/card-table.component";
+import {Card} from "../models/card";
+
+@Component({
+  selector: 'app-card-list',
+  standalone: true,
+  imports: [CommonModule, CardGridComponent, CardTableComponent],
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.scss'
+})
+export class CardListComponent {
+  @Input({ required: true }) selectedLayout!: string;
+  @Input({ required: true }) cardList!: Card[];
+  @Input({ required: true }) selectedCards!: Card[];
+  @Output() selectedCardsChange = new EventEmitter<Card[]>();
+  @HostBinding('class.p-border') borderApplied = true;
+}
