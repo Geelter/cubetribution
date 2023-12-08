19fa1713cdc62b4a70483e8bb51720840d943e0e
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 19:44:53 +0100

    Create a grid component for displaying collections

---
+++ src/app/collection-grid/collection-grid.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Collection} from "../models/collection";
+import {CollectionComponent} from "../collection/collection.component";
+import {sampleCollection} from "../models/collection";
+
+@Component({
+  selector: 'app-collection-grid',
+  standalone: true,
+  imports: [CommonModule, CollectionComponent],
+  templateUrl: './collection-grid.component.html',
+  styleUrl: './collection-grid.component.scss'
+})
+export class CollectionGridComponent {
+  // @Input({ required: true }) collectionList!: Collection[];
+  collectionList = Array(7).fill(sampleCollection);
+}
