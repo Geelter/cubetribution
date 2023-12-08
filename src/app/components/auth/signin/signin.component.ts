6458bf394c4b607da4d2d004e4000650404f0853
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 22:45:01 +0100

    Copy form from earlier project with needed adjustments

--- src/app/components/auth/signin/signin.component.ts
+++ src/app/components/auth/signin/signin.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {SupabaseAuthService} from "../../../services/supabase/supabase-auth.service";
+import {PasswordModule} from "primeng/password";
+import {ButtonModule} from "primeng/button";
+import {DividerModule} from "primeng/divider";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, PasswordModule, ButtonModule, DividerModule, InputTextModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+  formBuilder = inject(FormBuilder);
+  authService = inject(SupabaseAuthService);
 
+  signInForm: FormGroup;
+
+  isSubmitting: boolean = false;
+
+  async onSubmit() {
+    this.isSubmitting = true;
+
+    const { email, password } = this.signInForm.value;
+
+    await this.authService.signInWithEmail(email, password);
+
+    this.isSubmitting = false;
+  }
+
+  constructor() {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 }
