9b79f1fac0fb7fbd5239d54e51b278458ebc5d5c
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 19:54:20 +0100

    Replace map function with calling a Card class method

--- src/app/services/scryfall.service.ts
+++ src/app/services/scryfall.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Card, CardFace} from "../models/card";
+import {Card} from "../models/card";
 import {catchError, forkJoin, Observable, of, switchMap, tap, throwError} from "rxjs";
 import {ScryfallCollectionResponse} from "../models/scryfall-collection-response";
 import {ScryfallAutocompleteResponse} from "../models/scryfall-autocomplete-response";
@@ -32,14 +32,14 @@ export class ScryfallService {
     const urls = this.createRequestsForPayloads(payloads);
 
     return forkJoin(urls).pipe(
-      switchMap(responses =>
-        of(responses.flatMap(response => {
-          return response.data.map(value => this.mapResponseDataToCard(value))
-        }))
-      ),
       tap(response => console.log(response)),
+      switchMap(responses =>
+        of(responses.flatMap(response =>
+          response.data.map(value => new Card(value))
+        ))
+      ),
       catchError(
-        respose => throwError(() => new Error('Error fetching card collection'))
+        response => throwError(() => new Error('Error fetching card collection'))
       )
     );
   }
@@ -85,44 +85,5 @@ export class ScryfallService {
         { headers: {'Content-Type': 'application/json'} }
       )
     })
-  }
-
-  private mapResponseDataToCard(data: any): Card {
-    const cardData: Card = {
-      id: data.id,
-      name: data.name,
-      lang: data.lang,
-      released_at: data.released_at,
-      scryfall_uri: data.scryfall_uri,
-      layout: data.layout,
-      image_uris: data.image_uris ?? null,
-      card_faces: null,
-      mana_cost: data.mana_cost,
-      cmc: data.cmc,
-      type_line: data.type_line,
-      oracle_text: data.oracle_text,
-      power: data.power,
-      toughness: data.toughness,
-      colors: data.colors,
-      color_identity: data.color_identity,
-      rarity: data.rarity,
-    }
-
-    if (data.card_faces) {
-      cardData.card_faces = data.card_faces.map((cardFace: CardFace) => {
-        return {
-          colors: cardFace.colors,
-          mana_cost: cardFace.mana_cost,
-          name: cardFace.name,
-          oracle_text: cardFace.oracle_text,
-          power: cardFace.power,
-          toughness: cardFace.toughness,
-          type_line: cardFace.type_line,
-          image_uris: cardFace.image_uris ? cardFace.image_uris : null,
-        }
-      });
-    }
-
-    return cardData;
   }
 }
