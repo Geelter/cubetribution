af354d736791fcfedf7dae7c758a7aa8f77c87e2
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 19:58:38 +0100

    Replace navbar with PrimeNG menubar, add toast, wrap outlet in a section element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { BrowserComponent } from "./browser/browser.component";
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {MenuItem, PrimeNGConfig} from "primeng/api";
+import {MenubarModule} from "primeng/menubar";
+import {ThemeToggleComponent} from "./theme-toggle/theme-toggle.component";
+import {ToastModule} from "primeng/toast";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BrowserComponent],
+  imports: [CommonModule, RouterOutlet, MenubarModule, ThemeToggleComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  primengConfig = inject(PrimeNGConfig);
+
   title = 'cubetribution';
+
+  menuItems: MenuItem[];
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
+  constructor() {
+    this.menuItems = [
+      {
+        label: 'Browse',
+        routerLink: '/browse'
+      },
+      {
+        label: 'Cubes',
+        routerLink: '/cubes'
+      },
+      {
+        label: 'Collections',
+        routerLink: '/collections'
+      }
+    ];
+  }
 }
