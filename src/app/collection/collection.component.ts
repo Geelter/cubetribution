acba6db1974a6767ac43277a255754bc749f2121
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-08 19:42:03 +0100

    Create a collection card component

---
+++ src/app/collection/collection.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Collection} from "../models/collection";
+
+@Component({
+  selector: 'app-collection',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './collection.component.html',
+  styleUrl: './collection.component.scss'
+})
+export class CollectionComponent {
+  @Input({ required: true }) collection!: Collection;
+}
