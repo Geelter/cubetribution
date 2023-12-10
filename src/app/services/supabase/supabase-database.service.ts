bc76eb6bca0d0f7361e93af50f03fa1703b3c5f6
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-11 00:15:03 +0100

    Add methods for the rest of the CRUD operations

--- src/app/services/supabase/supabase-database.service.ts
+++ src/app/services/supabase/supabase-database.service.ts
@@ -2,6 +2,8 @@ import {inject, Injectable} from '@angular/core';
 import {SupabaseClientService} from "./supabase-client.service";
 import {Card} from "../../models/card";
 import {MessageService} from "primeng/api";
+import {Collection} from "../../models/collection";
+import {Tables} from "../../models/supabase";
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +12,15 @@ export class SupabaseDatabaseService {
   private readonly supabase = inject(SupabaseClientService);
   private readonly messageService = inject(MessageService);
 
-  async createCollection(name: string, cards: Card[] | null = null) {
-    let body: any = {};
-
-    body.name = name;
-    if (cards) {
-      body.card_ids = cards.map(card => card.id);
-    }
+  async createCollection(name: string, cards: Card[] = []) {
+    const cardIDs = cards.map(card => card.id);
 
     const { data, error } = await this.supabase.client
       .from('collections')
-      .insert(body)
-      .select();
+      .insert({ name: name, card_ids: cardIDs })
+      .select()
+      .returns<Tables<'collections'>>()
+      .single();
 
     if (error) {
       this.messageService.add({
@@ -30,15 +29,26 @@ export class SupabaseDatabaseService {
         summary: 'Creating collection failed',
         detail: error.message
       });
+    } else {
+      this.messageService.add({
+        key: 'global',
+        severity: 'success',
+        summary: 'Collection created'
+      });
     }
 
-    return data;
+    return data ? new Collection(data) : undefined;
   }
 
   async fetchCollections() {
     const { data, error } = await this.supabase.client
       .from('collections')
-      .select();
+      .select()
+      .returns<Tables<'collections'>[]>();
+
+    const collections = data
+      ? new Map(data.map(collection => [collection.id, new Collection(collection)]))
+      : undefined;
 
     if (error) {
       this.messageService.add({
@@ -48,8 +58,86 @@ export class SupabaseDatabaseService {
         detail: error.message
       });
       return;
+    } else {
+      this.messageService.add({
+        key: 'global',
+        severity: 'success',
+        summary: 'Collections fetched'
+      })
     }
 
-    return data;
+    return collections;
+  }
+
+  async deleteCollection(id: number) {
+    const { error } = await this.supabase.client
+      .from('collections')
+      .delete()
+      .eq('id', id);
+
+    if (error) {
+      this.messageService.add({
+        key: 'global',
+        severity: 'error',
+        summary: 'Deleting collection-card failed',
+        detail: error.message
+      });
+    } else {
+      this.messageService.add({
+        key: 'global',
+        severity: 'success',
+        summary: 'Collection deleted'
+      })
+    }
+
+    return error;
+  }
+
+  async updateCollectionMeta(id: number, changes: any) {
+    const { error } = await this.supabase.client
+      .from('collections')
+      .update(changes)
+      .eq('id', id);
+
+    if (error) {
+      this.messageService.add({
+        key: 'global',
+        severity: 'error',
+        summary: 'Updating collection-card failed',
+        detail: error.message
+      });
+    } else {
+      this.messageService.add({
+        key: 'global',
+        severity: 'success',
+        summary: 'Collection updated'
+      })
+    }
+
+    return error;
+  }
+
+  async updateCollectionCards(id: number, updatedIDs: string[]) {
+    const { error } = await this.supabase.client
+      .from('collections')
+      .update({ card_ids: updatedIDs })
+      .eq('id', id);
+
+    if (error) {
+      this.messageService.add({
+        key: 'global',
+        severity: 'error',
+        summary: 'Updating collection-card failed',
+        detail: error.message
+      });
+    } else {
+      this.messageService.add({
+        key: 'global',
+        severity: 'success',
+        summary: 'Collection updated'
+      })
+    }
+
+    return error;
   }
 }
