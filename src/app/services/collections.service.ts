6654150307defbfb7052a762f2826c63a15c8c91
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-11 01:25:19 +0100

    Add methods managing collection fetching

--- src/app/services/collections.service.ts
+++ src/app/services/collections.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {SupabaseDatabaseService} from "./supabase/supabase-database.service";
+import {BehaviorSubject, map} from "rxjs";
+import {Collection} from "../models/collection";
+import {Card} from "../models/card";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionsService {
+  private readonly databaseService = inject(SupabaseDatabaseService);
 
-  constructor() { }
+  private collections = new BehaviorSubject<Map<number, Collection> | null>(null);
+  collections$ = this.collections.asObservable()
+    .pipe(
+      map(collectionMap => {
+        return collectionMap
+          ? Array.from(collectionMap.values())
+          : null
+      })
+    );
+
+  private selectedCollection = new BehaviorSubject<Collection | null>(null);
+  selectedCollection$ = this.selectedCollection.asObservable();
+
+  async initializeCollections() {
+    const collectionsValue = this.collections.getValue();
+
+    if (!collectionsValue || !collectionsValue.size) {
+      await this.getCollections();
+    }
+  }
+
+  async getCollections() {
+    const collections = await this.databaseService.fetchCollections();
+
+    if (collections) {
+      this.collections.next(collections);
+    }
+  }
+
+  clearFetchedCollections() {
+    this.collections.next(null);
+  }
 }
