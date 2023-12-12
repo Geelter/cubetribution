da489a2634eb1d4013df0367f0400a0fc0e340b4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-13 00:53:46 +0100

    Mirror the collections service, making needed changes

--- src/app/services/cubes.service.ts
+++ src/app/services/cubes.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {SupabaseDatabaseService} from "./supabase/supabase-database.service";
+import {BehaviorSubject} from "rxjs";
+import {Cube} from "../models/cube";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CubesService {
+  private readonly databaseService = inject(SupabaseDatabaseService);
 
-  constructor() { }
+  private cubes = new BehaviorSubject<Cube[] | null>(null);
+  cubes$ = this.cubes.asObservable();
+
+  private selectedCube = new BehaviorSubject<Cube | null>(null);
+  selectedCube$ = this.selectedCube.asObservable();
+
+  async initializeCubes() {
+    const cubesValue = this.cubes.getValue();
+
+    if (!cubesValue || !cubesValue.length) {
+      await this.getCubes();
+    }
+  }
+
+  async getCubes() {
+    const cubes = await this.databaseService.fetchCubes();
+
+    if (cubes) {
+      this.cubes.next(cubes);
+    }
+  }
+
+  selectCube(cube: Cube) {
+    this.selectedCube.next(cube);
+  }
+
+  clearSelectedCube() {
+    this.selectedCube.next(null);
+  }
 }
