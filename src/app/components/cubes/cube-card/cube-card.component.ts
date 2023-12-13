06ff57e0ac7cfdef0ceb238dab93380aa1fe9697
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-13 19:05:23 +0100

    Generate components for cubes

---
+++ src/app/components/cubes/cube-card/cube-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cube-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cube-card.component.html',
+  styleUrl: './cube-card.component.scss'
+})
+export class CubeCardComponent {
+
+}
---
+++ src/app/components/cubes/cube/cube.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cube',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cube.component.html',
+  styleUrl: './cube.component.scss'
+})
+export class CubeComponent {
+
+}
