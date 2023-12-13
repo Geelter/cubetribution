8c206026a4410241bf75f01fea359c6664dbf977
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-13 19:12:52 +0100

    Mirror the collection card component with necessary changes

--- src/app/components/cubes/cube-card/cube-card.component.ts
+++ src/app/components/cubes/cube-card/cube-card.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Cube} from "../../../models/cube";
 
 @Component({
   selector: 'app-cube-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './cube-card.component.html',
   styleUrl: './cube-card.component.scss'
 })
 export class CubeCardComponent {
+  @Input({ required: true }) cube!: Cube;
+  @Output() cubeClicked = new EventEmitter<void>();
 
+  handleCubeClick() {
+    this.cubeClicked.emit();
+  }
 }
