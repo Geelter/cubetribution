7666af7e0f7ee5926808f1757de8daf17d671ad8
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-14 16:06:22 +0100

    Mirror the collection grid component with necessary changes

--- src/app/components/cubes/cube-grid/cube-grid.component.ts
+++ src/app/components/cubes/cube-grid/cube-grid.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router} from "@angular/router";
+import {CubesService} from "../../../services/cubes.service";
+import {Cube} from "../../../models/cube";
+import {ButtonModule} from "primeng/button";
+import {ToolbarModule} from "primeng/toolbar";
 
 @Component({
   selector: 'app-cube-grid',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, ToolbarModule],
   templateUrl: './cube-grid.component.html',
   styleUrl: './cube-grid.component.scss'
 })
-export class CubeGridComponent {
+export class CubeGridComponent implements OnInit {
+  private readonly router = inject(Router);
+  private readonly cubesService = inject(CubesService);
+  cubesList$ = this.cubesService.cubes$;
 
+  refreshCubes() {
+    this.cubesService.getCubes();
+  }
+
+  selectCube(cube: Cube) {
+    this.cubesService.selectCube(cube);
+    this.router.navigate(['cube-detail']);
+  }
+
+  ngOnInit() {
+    this.cubesService.initializeCubes();
+  }
 }
