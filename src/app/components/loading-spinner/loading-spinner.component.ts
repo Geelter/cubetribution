9c0ac85cea4ceac24eac529f45f633e6d1b67926
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-21 18:14:30 +0100

    Create a spinner component for indicating loading state

---
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ProgressSpinnerModule} from "primeng/progressspinner";
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [CommonModule, ProgressSpinnerModule],
+  templateUrl: './loading-spinner.component.html',
+  styleUrl: './loading-spinner.component.scss'
+})
+export class LoadingSpinnerComponent {
+}
