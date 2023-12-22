59f410abe15c8253b7e2cf6e92a8553e12c52e76
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-22 23:49:11 +0100

    Create a class for donation records

---
+++ src/app/models/donation.ts
@@ -0,0 +1,37 @@
+import {Tables} from "./supabase";
+
+export class Donation {
+  private readonly _id: number;
+  private readonly _cubeID: number;
+  private readonly _thumbnailURI: string;
+  private readonly _cardIDs: string[];
+  private readonly _accepted: boolean;
+
+  get id() {
+    return this._id;
+  }
+
+  get cubeID() {
+    return this._cubeID;
+  }
+
+  get thumbnailURI() {
+    return this._thumbnailURI;
+  }
+
+  get cardIDs() {
+    return this._cardIDs;
+  }
+
+  get accepted() {
+    return this._accepted;
+  }
+
+  constructor(data: Tables<'donations'>) {
+    this._id = data.id;
+    this._cubeID = data.cube_id;
+    this._thumbnailURI = data.thumbnail;
+    this._cardIDs = data.card_ids;
+    this._accepted = data.accepted;
+  }
+}
