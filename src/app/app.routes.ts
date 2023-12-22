c6755ba95c679f246e0ae41b158b939aed128e8e
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-22 16:25:13 +0100

    Rewrite router config with lazy loading and route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,89 @@
-import { Routes } from '@angular/router';
-import {CollectionGridComponent} from "./components/collections/collection-grid/collection-grid.component";
-import {BrowserComponent} from "./components/browser/browser.component";
-import {SigninComponent} from "./components/auth/signin/signin.component";
-import {SignupComponent} from "./components/auth/signup/signup.component";
-import {CollectionComponent} from "./components/collections/collection/collection.component";
+import {Routes} from '@angular/router';
+import {collectionDetailGuard} from "./guards/collection-detail.guard";
+import {authenticatedGuard} from "./guards/authenticated.guard";
+import {cubeDetailGuard} from "./guards/cube-detail.guard";
+import {unauthenticatedGuard} from "./guards/unauthenticated.guard";
 
 export const routes: Routes = [
   {
-    path: 'signin',
-    component: SigninComponent,
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'cube'
   },
   {
-    path: 'signup',
-    component: SignupComponent,
-  },
-  {
-    path: 'collections',
-    component: CollectionGridComponent,
-  },
-  {
-    path: 'collection-detail',
-    component: CollectionComponent,
+    path: 'auth',
+    canActivate: [unauthenticatedGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'signin',
+        pathMatch: 'full',
+      },
+      {
+        path: 'signin',
+        loadComponent: () => import('./components/auth/signin/signin.component').then(
+          m => m.SigninComponent
+        )
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./components/auth/signup/signup.component').then(
+          m => m.SignupComponent
+        )
+      }
+    ]
   },
   {
     path: 'browse',
-    component: BrowserComponent,
+    loadComponent: () => import('./components/browser/browser.component').then(
+      m => m.BrowserComponent
+    )
   },
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'collections'
+    path: 'collection',
+    canActivate: [authenticatedGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      },
+      {
+        path: 'list',
+        loadComponent: () => import('./components/collections/collection-grid/collection-grid.component').then(
+          m => m.CollectionGridComponent
+        )
+      },
+      {
+        path: 'detail',
+        canActivate: [collectionDetailGuard],
+        loadComponent: () => import('./components/collections/collection/collection.component').then(
+          m => m.CollectionComponent
+        )
+      }
+    ]
+  },
+  {
+    path: 'cube',
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      },
+      {
+        path: 'list',
+        loadComponent: () => import('./components/cubes/cube-grid/cube-grid.component').then(
+          m => m.CubeGridComponent
+        )
+      },
+      {
+        path: 'detail',
+        canActivate: [cubeDetailGuard],
+        loadComponent: () => import('./components/cubes/cube/cube.component').then(
+          m => m.CubeComponent
+        )
+      }
+    ]
   }
 ];
