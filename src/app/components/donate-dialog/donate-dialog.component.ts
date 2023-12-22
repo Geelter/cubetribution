36ce040243b6f5bd8b77d8461fa9f8cab32b18dd
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-22 22:35:20 +0100

    Generate the files for donate dialog

---
+++ src/app/components/donate-dialog/donate-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-donate-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './donate-dialog.component.html',
+  styleUrl: './donate-dialog.component.scss'
+})
+export class DonateDialogComponent {
+
+}
