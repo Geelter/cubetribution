7b02fa3e9604efe64c61c6f506969f065d6d59d4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-12-27 15:55:36 +0100

    Mirror the logic from collections service

---
+++ src/app/services/donations.service.ts
@@ -0,0 +1,57 @@
+import {inject, Injectable} from '@angular/core';
+import {SupabaseDatabaseService} from "./supabase/supabase-database.service";
+import {BehaviorSubject} from "rxjs";
+import {Donation} from "../models/donation";
+import {Collection} from "../models/collection";
+import {Cube} from "../models/cube";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DonationsService {
+  private readonly databaseService = inject(SupabaseDatabaseService);
+
+  private donations = new BehaviorSubject<Donation[] | null>(null);
+  donations$ = this.donations.asObservable();
+
+  private requestInProgress = new BehaviorSubject<boolean>(false);
+  requestInProgress$ = this.requestInProgress.asObservable();
+
+  async initializeDonations() {
+    const donationsValue = this.donations.getValue();
+
+    if (!donationsValue || !donationsValue.length) {
+      await this.getDonations();
+    }
+  }
+
+  async getDonations() {
+    this.requestInProgress.next(true);
+
+    const donations = await this.databaseService.fetchDonations();
+
+    this.requestInProgress.next(false);
+
+    if (donations) {
+      this.donations.next(donations);
+    }
+  }
+
+  clearFetchedCollections() {
+    this.donations.next(null);
+  }
+
+  async createDonation(collection: Collection, cube: Cube) {
+    this.requestInProgress.next(true);
+
+    const createdDonation = await this.databaseService.createDonationForCube(collection, cube);
+
+    if (createdDonation) {
+      const currentDonations = this.donations.getValue() ?? [];
+
+      this.donations.next([...currentDonations, createdDonation]);
+    }
+
+    this.requestInProgress.next(false);
+  }
+}
