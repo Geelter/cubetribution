a6946b792482c3b363943d79abc7fb34a748ed8f
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-04 17:22:51 +0100

    Define view model for donation component

--- src/app/components/donations/donation/donation.component.ts
+++ src/app/components/donations/donation/donation.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DonationsService} from "../../../services/donations.service";
+import {CardsService} from "../../../services/cards.service";
+import {combineLatestWith, map, Observable, switchMap} from "rxjs";
+import {Donation} from "../../../models/donation";
+import {Card} from "../../../models/card";
 
 @Component({
   selector: 'app-donation',
@@ -9,5 +14,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './donation.component.scss'
 })
 export class DonationComponent {
+  private readonly donationsService = inject(DonationsService);
+  private readonly cardsService = inject(CardsService);
 
+  private readonly donation$: Observable<Donation | null>;
+  private readonly donationCards$: Observable<Card[]>;
+  vm$: Observable<{donation: Donation | null, donationCards: Card[]}>;
+
+  constructor() {
+    this.donation$ = this.donationsService.selectedDonation$;
+    this.donationCards$ = this.donationsService.selectedDonation$.pipe(
+      switchMap(donation =>
+        this.cardsService.getCardsForIDs(donation?.cardIDs ?? [])
+      )
+    );
+
+    this.vm$ = this.donation$.pipe(
+      combineLatestWith(this.donationCards$),
+      map(([donation, donationCards]) => ({ donation, donationCards }))
+    );
+  }
 }
