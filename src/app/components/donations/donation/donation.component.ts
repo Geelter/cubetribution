afe4d854458a21d649d4414eb4bc84d943d7c6ad
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-04 20:12:49 +0100

    Add toolbar and its buttons to donation component

--- src/app/components/donations/donation/donation.component.ts
+++ src/app/components/donations/donation/donation.component.ts
@@ -5,21 +5,48 @@ import {CardsService} from "../../../services/cards.service";
 import {combineLatestWith, map, Observable, switchMap} from "rxjs";
 import {Donation} from "../../../models/donation";
 import {Card} from "../../../models/card";
+import {ToolbarModule} from "primeng/toolbar";
+import {ConfirmationService, SharedModule} from "primeng/api";
+import {layoutOptions} from "../../../app.config";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {ButtonModule} from "primeng/button";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-donation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ToolbarModule, SharedModule, SelectButtonModule, ButtonModule, FormsModule],
   templateUrl: './donation.component.html',
   styleUrl: './donation.component.scss'
 })
 export class DonationComponent {
+  protected readonly layoutOptions = layoutOptions;
   private readonly donationsService = inject(DonationsService);
+  private readonly confirmationService = inject(ConfirmationService);
   private readonly cardsService = inject(CardsService);
 
   private readonly donation$: Observable<Donation | null>;
   private readonly donationCards$: Observable<Card[]>;
   vm$: Observable<{donation: Donation | null, donationCards: Card[]}>;
+
+  selectedCards: Card[] = [];
+
+  selectedLayout: string = 'grid';
+
+  confirmDelete(donation: Donation) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to remove selected cards?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.deleteSelectedCards(donation);
+      }
+    });
+  }
+
+  deleteSelectedCards(donation: Donation) {
+    this.donationsService.removeCardsFromDonation(donation, this.selectedCards).then(() => this.selectedCards = []);
+  }
 
   constructor() {
     this.donation$ = this.donationsService.selectedDonation$;
