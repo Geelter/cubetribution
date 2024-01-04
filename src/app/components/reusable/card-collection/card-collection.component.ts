8c83157658b23a3c3b619806eceb8027b09178de
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-04 02:20:03 +0100

    Create a simple reusable structure fillable through content projection

--- src/app/components/reusable/card-collection/card-collection.component.ts
+++ src/app/components/reusable/card-collection/card-collection.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ToolbarModule} from "primeng/toolbar";
+import {SharedModule} from "primeng/api";
 
 @Component({
   selector: 'app-card-collection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ToolbarModule, SharedModule],
   templateUrl: './card-collection.component.html',
   styleUrl: './card-collection.component.scss'
 })
