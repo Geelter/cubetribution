8bbec5c44758c3b1f9ccf50d0a9aec58e328f107
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-04 20:20:53 +0100

    Mirror guard logic from collection detail guard

--- src/app/guards/donation-detail.guard.ts
+++ src/app/guards/donation-detail.guard.ts
@@ -1,5 +1,15 @@
-import {CanActivateFn} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {DonationsService} from "../services/donations.service";
+import {switchMap} from "rxjs";
 
-export const donationDetailGuard: CanActivateFn = (route, state) => {
-  return true;
+export const donationDetailGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const donationsService = inject(DonationsService);
+
+  return donationsService.selectedDonation$.pipe(
+    switchMap(async (selectedDonation) =>
+      selectedDonation ? true : router.parseUrl('/donation/list')
+    )
+  );
 };
