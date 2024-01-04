b610273e5f0b18a11b8d2bfab609712fd25d9c73
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-04 02:06:25 +0100

    Generate files for card collection component

---
+++ src/app/components/reusable/card-collection/card-collection.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-collection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-collection.component.html',
+  styleUrl: './card-collection.component.scss'
+})
+export class CardCollectionComponent {
+
+}
