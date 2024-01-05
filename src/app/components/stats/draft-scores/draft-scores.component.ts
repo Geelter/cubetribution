274762a63ca0f8d1f4523055720aafc01cec41e2
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-05 08:18:08 +0100

    Generate files for draft scores component

---
+++ src/app/components/stats/draft-scores/draft-scores.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-draft-scores',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './draft-scores.component.html',
+  styleUrl: './draft-scores.component.scss'
+})
+export class DraftScoresComponent {
+
+}
