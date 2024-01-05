36e945893d3fec8ed4545eda674e98e7b698f857
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-05 06:03:47 +0100

    Rewrite routing configuration for breadcrumbs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,12 +8,16 @@ import {donationDetailGuard} from "./guards/donation-detail.guard";
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'cube'
+    redirectTo: 'browser',
+    pathMatch: 'full'
   },
   {
     path: 'auth',
     canActivate: [unauthenticatedGuard],
+    data: {
+      title: 'Authentication',
+      breadcrumb: 'Authentication'
+    },
     children: [
       {
         path: '',
@@ -24,90 +28,159 @@ export const routes: Routes = [
         path: 'signin',
         loadComponent: () => import('./components/auth/signin/signin.component').then(
           m => m.SigninComponent
-        )
+        ),
+        data: {
+          title: 'Sign In',
+          breadcrumb: 'Sign In'
+        }
       },
       {
         path: 'signup',
         loadComponent: () => import('./components/auth/signup/signup.component').then(
           m => m.SignupComponent
-        )
+        ),
+        data: {
+          title: 'Sign Up',
+          breadcrumb: 'Sign Up'
+        }
       }
     ]
   },
   {
-    path: 'browse',
+    path: 'browser',
     loadComponent: () => import('./components/browser/browser.component').then(
       m => m.BrowserComponent
-    )
+    ),
+    data: {
+      title: 'Card Browser',
+      breadcrumb: 'Browser'
+    }
   },
   {
-    path: 'collection',
+    path: 'cubes',
+    data: {
+      breadcrumb: 'Cubes'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'list',
+        data: {
+          title: 'Cubes List',
+          breadcrumb: 'List'
+        },
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./components/cubes/cube-grid/cube-grid.component').then(
+              m => m.CubeGridComponent
+            ),
+            data: {
+              breadcrumb: null
+            }
+          },
+          {
+            path: 'detail',
+            canActivate: [cubeDetailGuard],
+            data: {
+              title: 'Cube Detail',
+              breadcrumb: 'Detail'
+            },
+            loadComponent: () => import('./components/cubes/cube/cube.component').then(
+              m => m.CubeComponent
+            )
+          }
+        ]
+      }
+    ],
+  },
+  {
+    path: 'collections',
     canActivate: [authenticatedGuard],
+    data: {
+      breadcrumb: 'Collections'
+    },
     children: [
       {
         path: '',
         redirectTo: 'list',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'list',
-        loadComponent: () => import('./components/collections/collection-grid/collection-grid.component').then(
-          m => m.CollectionGridComponent
-        )
-      },
-      {
-        path: 'detail',
-        canActivate: [collectionDetailGuard],
-        loadComponent: () => import('./components/collections/collection/collection.component').then(
-          m => m.CollectionComponent
-        )
+        data: {
+          title: 'Collections List',
+          breadcrumb: 'List'
+        },
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./components/collections/collection-grid/collection-grid.component').then(
+              m => m.CollectionGridComponent
+            ),
+            data: {
+              breadcrumb: null
+            }
+          },
+          {
+            path: 'detail',
+            canActivate: [collectionDetailGuard],
+            data: {
+              title: 'Collection Detail',
+              breadcrumb: 'Detail'
+            },
+            loadComponent: () => import('./components/collections/collection/collection.component').then(
+              m => m.CollectionComponent
+            )
+          }
+        ]
       }
-    ]
+    ],
   },
   {
-    path: 'cube',
+    path: 'donations',
+    data: {
+      breadcrumb: 'Donations'
+    },
     children: [
       {
         path: '',
         redirectTo: 'list',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'list',
-        loadComponent: () => import('./components/cubes/cube-grid/cube-grid.component').then(
-          m => m.CubeGridComponent
-        )
-      },
-      {
-        path: 'detail',
-        canActivate: [cubeDetailGuard],
-        loadComponent: () => import('./components/cubes/cube/cube.component').then(
-          m => m.CubeComponent
-        )
+        data: {
+          title: 'Donations List',
+          breadcrumb: 'List'
+        },
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./components/donations/donation-grid/donation-grid.component').then(
+              m => m.DonationGridComponent
+            ),
+            data: {
+              breadcrumb: null
+            }
+          },
+          {
+            path: 'detail',
+            canActivate: [donationDetailGuard],
+            data: {
+              title: 'Donation Detail',
+              breadcrumb: 'Detail'
+            },
+            loadComponent: () => import('./components/donations/donation/donation.component').then(
+              m => m.DonationComponent
+            )
+          }
+        ]
       }
-    ]
-  },
-  {
-    path: 'donation',
-    children: [
-      {
-        path: '',
-        redirectTo: 'list',
-        pathMatch: 'full'
-      },
-      {
-        path: 'list',
-        loadComponent: () => import('./components/donations/donation-grid/donation-grid.component').then(
-          m => m.DonationGridComponent
-        )
-      },
-      {
-        path: 'detail',
-        canActivate: [donationDetailGuard],
-        loadComponent: () => import('./components/donations/donation/donation.component').then(
-          m => m.DonationComponent
-        )
-      }
-    ]
+    ],
   }
 ];
