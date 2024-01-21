169bbb2d1debad2dc095600c928ca51525bfa3c4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-22 00:20:47 +0100

    Refactor cube grid component, so it matches the collection grid component

--- src/app/components/cubes/cube-grid/cube-grid.component.ts
+++ src/app/components/cubes/cube-grid/cube-grid.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Router} from "@angular/router";
 import {CubesService} from "../../../services/cubes.service";
@@ -6,21 +6,64 @@ import {Cube} from "../../../models/cube";
 import {ButtonModule} from "primeng/button";
 import {ToolbarModule} from "primeng/toolbar";
 import {CubeCardComponent} from "../cube-card/cube-card.component";
+import {catchError, take, throwError} from "rxjs";
+import {RequestState} from "../../../helpers/request-state.enum";
+import {LoadingSpinnerComponent} from "../../loading-spinner/loading-spinner.component";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {dialogBreakpoints} from "../../../app.config";
+import {ConfirmationService, MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-cube-grid',
   standalone: true,
-  imports: [CommonModule, ButtonModule, ToolbarModule, CubeCardComponent],
+  imports: [CommonModule, ButtonModule, ToolbarModule, CubeCardComponent, LoadingSpinnerComponent, ConfirmDialogModule],
   templateUrl: './cube-grid.component.html',
-  styleUrl: './cube-grid.component.scss'
+  styleUrl: './cube-grid.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CubeGridComponent implements OnInit {
+export class CubeGridComponent {
+  protected readonly RequestState = RequestState;
+  protected readonly dialogBreakpoints = dialogBreakpoints;
   private readonly router = inject(Router);
+  private readonly confirmationService = inject(ConfirmationService);
+  private readonly messageService = inject(MessageService);
   private readonly cubesService = inject(CubesService);
+
   cubesList$ = this.cubesService.cubes$;
+  cubesRequestState$ = this.cubesService.requestState$;
+
+  showErrorDialog(error: Error) {
+    this.confirmationService.confirm({
+      message: error.message,
+      header: 'Error fetching cubes',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.cubesService.fetchCubes().pipe(
+          take(1),
+          catchError(error => {
+            this.showErrorDialog(error);
+            return throwError(() => new Error(error));
+          })
+        ).subscribe({
+          complete: (() => this.showSuccessMessage('Cubes fetched'))
+        });
+      },
+      reject: () => {
+        this.router.navigate(['..']);
+      }
+    })
+  }
 
   refreshCubes() {
-    this.cubesService.getCubes();
+    this.cubesService.fetchCubes().pipe(
+      take(1),
+      catchError(error => {
+        this.showErrorMessage('Fetching cubes failed', error.message);
+        return throwError(() => new Error(error));
+      })
+    ).subscribe({
+      complete: (() => this.showSuccessMessage('Cubes refreshed'))
+    });
   }
 
   selectCube(cube: Cube) {
@@ -28,7 +71,30 @@ export class CubeGridComponent implements OnInit {
     this.router.navigate(['/cubes', 'list', 'detail']);
   }
 
-  ngOnInit() {
-    this.cubesService.initializeCubes();
+  showSuccessMessage(summary: string) {
+    this.messageService.add({
+      key: 'global',
+      severity: 'success',
+      summary: summary
+    })
+  }
+
+  showErrorMessage(summary: string, detail: string = '') {
+    this.messageService.add({
+      key: 'global',
+      severity: 'error',
+      summary: summary,
+      detail: detail
+    })
+  }
+
+  constructor() {
+    this.cubesService.getCubes().pipe(
+      take(1),
+      catchError(error => {
+        this.showErrorDialog(error);
+        return throwError(() => new Error(error));
+      })
+    ).subscribe();
   }
 }
