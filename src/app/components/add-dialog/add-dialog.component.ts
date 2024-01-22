debacbec3b563c1b2e35ca5ac85c15711a8c0cb4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-22 04:05:15 +0100

    Refactor both dialog components so that they can handle request errors

--- src/app/components/add-dialog/add-dialog.component.ts
+++ src/app/components/add-dialog/add-dialog.component.ts
@@ -7,33 +7,74 @@ import {DialogModule} from "primeng/dialog";
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {InputTextModule} from "primeng/inputtext";
 import {ListboxModule} from "primeng/listbox";
-import {SharedModule} from "primeng/api";
+import {ConfirmationService, MessageService, SharedModule} from "primeng/api";
 import {Card} from "../../models/card";
 import {DividerModule} from "primeng/divider";
 import {dialogBreakpoints} from "../../app.config";
+import {catchError, take, throwError} from "rxjs";
+import {Router} from "@angular/router";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {RequestState} from "../../helpers/request-state.enum";
+import {LoadingSpinnerComponent} from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-add-dialog',
   standalone: true,
-  imports: [CommonModule, ButtonModule, DialogModule, FormsModule, InputTextModule, ListboxModule, ReactiveFormsModule, SharedModule, DividerModule],
+  imports: [CommonModule, ButtonModule, DialogModule, FormsModule, InputTextModule, ListboxModule, ReactiveFormsModule, SharedModule, DividerModule, ConfirmDialogModule, LoadingSpinnerComponent],
   templateUrl: './add-dialog.component.html',
   styleUrl: './add-dialog.component.scss'
 })
 export class AddDialogComponent {
-  protected readonly dialogBreakpoints = dialogBreakpoints;
   @Input({ required: true }) dialogVisible!: boolean;
   @Input({ required: true }) cardSelection!: Card[];
   @Output() dialogVisibleChange = new EventEmitter<boolean>();
+  protected readonly RequestState = RequestState;
+  protected readonly dialogBreakpoints = dialogBreakpoints;
   private readonly formBuilder = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private readonly messageService = inject(MessageService);
+  private readonly confirmationService = inject(ConfirmationService);
   private readonly collectionsService = inject(CollectionsService);
 
   collectionForm: FormGroup;
   collections$ = this.collectionsService.collections$;
+  collectionsRequestState$ = this.collectionsService.requestState$;
   selectedCollection: Collection | null = null;
+
+  showErrorDialog(error: Error) {
+    this.confirmationService.confirm({
+      message: error.message,
+      header: 'Error fetching collections',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.collectionsService.fetchCollections().pipe(
+          take(1),
+          catchError(error => {
+            this.showErrorDialog(error);
+            return throwError(() => new Error(error));
+          })
+        ).subscribe({
+          complete: (() => this.showSuccessMessage('Collections fetched'))
+        });
+      },
+      reject: () => {
+        this.router.navigate(['..']);
+      }
+    })
+  }
 
   addSelectionToCollection() {
     if (this.selectedCollection) {
-      this.collectionsService.addCardsToCollection(this.selectedCollection, this.cardSelection);
+      this.collectionsService.addCardsToCollection(this.selectedCollection, this.cardSelection)
+        .pipe(
+          take(1),
+          catchError(error => {
+            this.showErrorMessage('Adding cards to collection failed', error.message);
+            return throwError(() => new Error(error.message))
+          })
+        ).subscribe({
+        complete: (() => this.showSuccessMessage('Cards added to collection'))
+      });
     }
 
     this.dialogVisibleChange.emit(false);
@@ -42,10 +83,36 @@ export class AddDialogComponent {
   createCollectionWithSelection() {
     const { name } = this.collectionForm.value;
 
-    this.collectionsService.createCollection(name, this.cardSelection);
+    this.collectionsService.createCollection(name, this.cardSelection)
+      .pipe(
+        take(1),
+        catchError(error => {
+          this.showErrorMessage('Creating collection failed', error.message);
+          return throwError(() => new Error(error));
+        })
+      ).subscribe({
+      complete: (() => this.showSuccessMessage('Cards added to collection'))
+    });
 
     this.collectionForm.reset();
     this.dialogVisibleChange.emit(false);
+  }
+
+  private showSuccessMessage(summary: string) {
+    this.messageService.add({
+      key: 'global',
+      severity: 'success',
+      summary: summary
+    })
+  }
+
+  private showErrorMessage(summary: string, detail: string = '') {
+    this.messageService.add({
+      key: 'global',
+      severity: 'error',
+      summary: summary,
+      detail: detail
+    })
   }
 
   constructor() {
@@ -53,6 +120,12 @@ export class AddDialogComponent {
       name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(60)]]
     });
 
-    this.collectionsService.initializeCollections();
+    this.collectionsService.getCollections().pipe(
+      take(1),
+      catchError(error => {
+        this.showErrorDialog(error);
+        return throwError(() => new Error(error));
+      })
+    ).subscribe();
   }
 }
--- src/app/components/donate-dialog/donate-dialog.component.ts
+++ src/app/components/donate-dialog/donate-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DialogModule} from "primeng/dialog";
 import {CubesService} from "../../services/cubes.service";
@@ -8,33 +8,101 @@ import {Collection} from "../../models/collection";
 import {ListboxModule} from "primeng/listbox";
 import {FormsModule} from "@angular/forms";
 import {ButtonModule} from "primeng/button";
+import {catchError, take, throwError} from "rxjs";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {dialogBreakpoints} from "../../app.config";
+import {ConfirmationService, MessageService} from "primeng/api";
+import {Router} from "@angular/router";
+import {RequestState} from "../../helpers/request-state.enum";
+import {LoadingSpinnerComponent} from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-donate-dialog',
   standalone: true,
-  imports: [CommonModule, DialogModule, ListboxModule, FormsModule, ButtonModule],
+  imports: [CommonModule, DialogModule, ListboxModule, FormsModule, ButtonModule, ConfirmDialogModule, LoadingSpinnerComponent],
   templateUrl: './donate-dialog.component.html',
-  styleUrl: './donate-dialog.component.scss'
+  styleUrl: './donate-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DonateDialogComponent {
   @Input({ required: true }) dialogVisible!: boolean;
   @Input({ required: true }) collection!: Collection;
   @Output() dialogVisibleChange = new EventEmitter<boolean>();
+  protected readonly RequestState = RequestState;
+  protected readonly dialogBreakpoints = dialogBreakpoints;
+  private readonly router = inject(Router);
+  private readonly confirmationService = inject(ConfirmationService);
+  private readonly messageService = inject(MessageService);
   private readonly cubesService = inject(CubesService);
   private readonly donationsService = inject(DonationsService);
 
   cubes$ = this.cubesService.cubes$;
+  cubesRequestState$ = this.cubesService.requestState$;
+  donationsRequestState$ = this.donationsService.requestState$;
   selectedCube: Cube | null = null;
+
+  showErrorDialog(error: Error) {
+    this.confirmationService.confirm({
+      message: error.message,
+      header: 'Error fetching cubes',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.cubesService.fetchCubes().pipe(
+          take(1),
+          catchError(error => {
+            this.showErrorDialog(error);
+            return throwError(() => new Error(error));
+          })
+        ).subscribe({
+          complete: (() => this.showSuccessMessage('Cubes fetched'))
+        });
+      },
+      reject: () => {
+        this.router.navigate(['..']);
+      }
+    })
+  }
 
   donateCollectionToCube(collection: Collection, cube: Cube) {
     if (this.selectedCube) {
-      this.donationsService.createDonation(collection, cube);
+      this.donationsService.createDonation(collection, cube).pipe(
+        take(1),
+        catchError(error => {
+          this.showErrorMessage('Creating donation failed', error.message);
+          return throwError(() => new Error(error));
+        })
+      ).subscribe({
+        complete: (() => this.showSuccessMessage('Donation created'))
+      });
     }
 
     this.dialogVisibleChange.emit(false);
   }
 
+  private showSuccessMessage(summary: string) {
+    this.messageService.add({
+      key: 'global',
+      severity: 'success',
+      summary: summary
+    })
+  }
+
+  private showErrorMessage(summary: string, detail: string = '') {
+    this.messageService.add({
+      key: 'global',
+      severity: 'error',
+      summary: summary,
+      detail: detail
+    })
+  }
+
   constructor() {
-    this.cubesService.initializeCubes();
+    this.cubesService.getCubes().pipe(
+      take(1),
+      catchError(error => {
+        this.showErrorDialog(error);
+        return throwError(() => new Error(error));
+      })
+    ).subscribe();
   }
 }
