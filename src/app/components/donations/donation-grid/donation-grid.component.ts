59bfc231c153a0394a94f80e156b7c9c69c6372f
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-22 04:01:44 +0100

    Refactor donation grid component, so it matches the collection grid component

--- src/app/components/donations/donation-grid/donation-grid.component.ts
+++ src/app/components/donations/donation-grid/donation-grid.component.ts
@@ -1,7 +1,7 @@
-import {Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ToolbarModule} from "primeng/toolbar";
-import {ConfirmationService, SharedModule} from "primeng/api";
+import {ConfirmationService, MessageService, SharedModule} from "primeng/api";
 import {ButtonModule} from "primeng/button";
 import {SelectButtonModule} from "primeng/selectbutton";
 import {FormsModule} from "@angular/forms";
@@ -11,19 +11,24 @@ import {Router} from "@angular/router";
 import {DonationCardComponent} from "../donation-card/donation-card.component";
 import {ConfirmDialogModule} from "primeng/confirmdialog";
 import {dialogBreakpoints} from "../../../app.config";
-import {map} from "rxjs";
+import {catchError, map, take, throwError} from "rxjs";
+import {RequestState} from "../../../helpers/request-state.enum";
+import {LoadingSpinnerComponent} from "../../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-donation-grid',
   standalone: true,
-  imports: [CommonModule, ToolbarModule, SharedModule, ButtonModule, SelectButtonModule, FormsModule, DonationCardComponent, ConfirmDialogModule],
+  imports: [CommonModule, ToolbarModule, SharedModule, ButtonModule, SelectButtonModule, FormsModule, DonationCardComponent, ConfirmDialogModule, LoadingSpinnerComponent],
   templateUrl: './donation-grid.component.html',
-  styleUrl: './donation-grid.component.scss'
+  styleUrl: './donation-grid.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DonationGridComponent {
   protected readonly dialogBreakpoints = dialogBreakpoints;
+  protected readonly RequestState = RequestState;
   private readonly router = inject(Router);
   private readonly confirmationService = inject(ConfirmationService);
+  private readonly messageService = inject(MessageService);
   private readonly donationsService = inject(DonationsService);
   private readonly donationList$ = this.donationsService.donations$;
 
@@ -34,6 +39,30 @@ export class DonationGridComponent {
   readonly acceptedDonations$ = this.donationList$.pipe(
     map(donations => donations?.filter(donation => donation.accepted) ?? [])
   );
+
+  donationsRequestState$ = this.donationsService.requestState$;
+
+  showErrorDialog(error: Error) {
+    this.confirmationService.confirm({
+      message: error.message,
+      header: 'Error fetching donations',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.donationsService.fetchDonations().pipe(
+          take(1),
+          catchError(error => {
+            this.showErrorDialog(error);
+            return throwError(() => new Error(error));
+          })
+        ).subscribe({
+          complete: (() => this.showSuccessMessage('Donations fetched'))
+        });
+      },
+      reject: () => {
+        this.router.navigate(['..']);
+      }
+    })
+  }
 
   selectedDonationStatus: string = 'pending';
   donationStatusOptions = [
@@ -50,7 +79,15 @@ export class DonationGridComponent {
   }
 
   refreshDonations() {
-    this.donationsService.getDonations();
+    this.donationsService.fetchDonations().pipe(
+      take(1),
+      catchError(error => {
+        this.showErrorMessage('Fetching donations failed', error.message);
+        return throwError(() => new Error(error));
+      })
+    ).subscribe({
+      complete: (() => this.showSuccessMessage('Donations refreshed'))
+    });
   }
 
   selectDonation(donation: Donation) {
@@ -64,20 +101,47 @@ export class DonationGridComponent {
       header: 'Confirmation',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.deleteDonation(donation);
+        this.donationsService.deleteDonation(donation).pipe(
+          take(1),
+          catchError(error => {
+            this.showErrorMessage('Deleting donation failed', error.message);
+            return throwError(() => new Error(error));
+          })
+        ).subscribe({
+          complete: (() => this.showSuccessMessage('Donation deleted'))
+        });
       }
     });
-  }
-
-  private deleteDonation(donation: Donation) {
-    this.donationsService.deleteDonation(donation);
   }
 
   handleOptionClick(event: any) {
     if (event.option.status == 'accepted') this.deleteMode = false;
   }
 
+  private showSuccessMessage(summary: string) {
+    this.messageService.add({
+      key: 'global',
+      severity: 'success',
+      summary: summary
+    })
+  }
+
+  private showErrorMessage(summary: string, detail: string = '') {
+    this.messageService.add({
+      key: 'global',
+      severity: 'error',
+      summary: summary,
+      detail: detail
+    })
+  }
+
   constructor() {
-    this.donationsService.initializeDonations();
+    this.donationsService.getDonations().pipe(
+      take(1),
+      catchError(error => {
+        this.showErrorDialog(error);
+        return throwError(() => new Error(error));
+      })
+    ).subscribe();
   }
 }
