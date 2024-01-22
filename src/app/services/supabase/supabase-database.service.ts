89a7b1e6dbfbf5b70dde7dc2bb63d3384bfeb8a4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-22 04:09:37 +0100

    Remove unneeded code from supabase database service. Add fetch methods for draft related data.

--- src/app/services/supabase/supabase-database.service.ts
+++ src/app/services/supabase/supabase-database.service.ts
@@ -1,11 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {SupabaseClientService} from "./supabase-client.service";
-import {Card} from "../../models/card";
 import {MessageService} from "primeng/api";
-import {Collection} from "../../models/collection";
 import {Tables} from "../../models/supabase";
-import {Cube} from "../../models/cube";
-import {Donation} from "../../models/donation";
 
 @Injectable({
   providedIn: 'root'
@@ -14,266 +10,60 @@ export class SupabaseDatabaseService {
   private readonly supabase = inject(SupabaseClientService);
   private readonly messageService = inject(MessageService);
 
-  // Collections
-  async createCollection(name: string, cards: Card[] = []) {
-    const cardIDs = cards.map(card => card.id);
-
+  // Drafts
+  private async fetchDrafts() {
     const { data, error } = await this.supabase.client
-      .from('collections')
-      .insert({ name: name, card_ids: cardIDs })
+      .from('drafts')
       .select()
-      .returns<Tables<'collections'>>()
-      .single();
+      .returns<Tables<'drafts'>[]>();
 
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Creating collection failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Collection created'
-      });
-    }
-
-    return data ? new Collection(data) : undefined;
+    return data;
   }
 
-  async fetchCollections() {
+  // Players
+  private async fetchPlayers() {
     const { data, error } = await this.supabase.client
-      .from('collections')
+      .from('players')
       .select()
-      .returns<Tables<'collections'>[]>();
+      .returns<Tables<'players'>[]>();
 
-    const collections = data
-      ? new Map(data.map(collection => [collection.id, new Collection(collection)]))
-      : undefined;
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Fetching collections failed',
-        detail: error.message
-      });
-      return;
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Collections fetched'
-      })
-    }
-
-    return collections;
+    return data;
   }
 
-  async deleteCollection(id: number) {
-    const { error } = await this.supabase.client
-      .from('collections')
-      .delete()
-      .eq('id', id);
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Deleting collection failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Collection deleted'
-      })
-    }
-
-    return error;
-  }
-
-  async updateCollectionMeta(id: number, changes: any) {
-    const { error } = await this.supabase.client
-      .from('collections')
-      .update(changes)
-      .eq('id', id);
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Updating collection-card failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Collection updated'
-      })
-    }
-
-    return error;
-  }
-
-  async updateCollectionCards(id: number, updatedIDs: string[]) {
-    const { error } = await this.supabase.client
-      .from('collections')
-      .update({ card_ids: updatedIDs })
-      .eq('id', id);
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Updating collection-card failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Collection updated'
-      })
-    }
-
-    return error;
-  }
-
-  // Cubes
-  async fetchCubes() {
+  // Scores
+  private async fetchScores() {
     const { data, error } = await this.supabase.client
-      .from('cubes')
+      .from('scores')
       .select()
-      .returns<Tables<'cubes'>[]>();
+      .returns<Tables<'scores'>[]>();
 
-    const cubes = data?.map(cube => new Cube(cube));
 
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Fetching cubes failed',
-        detail: error.message
-      });
-      return;
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Cubes fetched'
-      })
-    }
-
-    return cubes;
+    return data;
   }
 
-  // Donations
-  async createDonationForCube(collection: Collection, cube: Cube) {
-    const filteredCards = collection.cardIDs.filter(id => cube.cardIDs.includes(id));
+  async fetchDashboardData() {
+    const drafts = await this.fetchDrafts();
+    const players = await this.fetchPlayers();
+    const scores = await this.fetchScores();
 
-    const { data, error } = await this.supabase.client
-      .from('donations')
-      .insert({ name: `${cube.name} donation`, cube_id: cube.id, card_ids: filteredCards })
-      .select()
-      .returns<Tables<'donations'>>()
-      .single();
-
-    if (error) {
+    if (!drafts || !players || !scores) {
       this.messageService.add({
         key: 'global',
         severity: 'error',
-        summary: 'Creating donation failed',
-        detail: error.message
+        summary: 'Fetching dashboard data failed',
       });
     } else {
       this.messageService.add({
         key: 'global',
         severity: 'success',
-        summary: 'Donation created'
+        summary: 'Dashboard data fetched'
       });
     }
 
-    return data ? new Donation(data) : undefined;
-  }
-
-  async deleteDonation(donation: Donation) {
-    const { error } = await this.supabase.client
-      .from('donations')
-      .delete()
-      .eq('id', donation.id);
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Deleting failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Donation deleted'
-      });
+    return {
+      drafts: drafts,
+      players: players,
+      scores: scores
     }
-
-    return error;
-  }
-
-  async fetchDonations() {
-    const { data, error } = await this.supabase.client
-      .from('donations')
-      .select()
-      .returns<Tables<'donations'>[]>();
-
-    const donations = data
-      ? new Map(data.map(donation => [donation.id, new Donation(donation)]))
-      : undefined;
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Fetching donations failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Donations fetched'
-      });
-    }
-
-    return donations;
-  }
-
-  async updateDonationCards(id: number, updatedIDs: string[]) {
-    const { error } = await this.supabase.client
-      .from('donations')
-      .update({ card_ids: updatedIDs })
-      .eq('id', id);
-
-    if (error) {
-      this.messageService.add({
-        key: 'global',
-        severity: 'error',
-        summary: 'Updating donation failed',
-        detail: error.message
-      });
-    } else {
-      this.messageService.add({
-        key: 'global',
-        severity: 'success',
-        summary: 'Donation updated'
-      })
-    }
-
-    return error;
   }
 }
