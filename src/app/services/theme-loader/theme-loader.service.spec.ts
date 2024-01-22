457494366b7a82def9d38881f16ba23924f48056
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-23 00:45:18 +0100

    Write tests for theme loader service

--- src/app/services/theme-loader/theme-loader.service.spec.ts
+++ src/app/services/theme-loader/theme-loader.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ThemeLoaderService } from './theme-loader.service';
+import {ThemeLoaderService} from './theme-loader.service';
+import {ThemeMode} from "./theme-loader.service";
 
 describe('ThemeLoaderService', () => {
-  let service: ThemeLoaderService;
+  let themeLoaderService: ThemeLoaderService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ThemeLoaderService);
+    themeLoaderService = TestBed.inject(ThemeLoaderService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(themeLoaderService).toBeTruthy();
+  });
+
+  it('should change the href attribute when given a valid theme mode', () => {
+    const linkElement = document.createElement('link');
+    linkElement.id = themeLoaderService['linkID'];
+    document.body.appendChild(linkElement);
+
+    themeLoaderService.switchTheme(ThemeMode.Light);
+
+    const expectedPath = `${window.location.origin}/assets/primeng/themes/lara/lara-light/amber/theme.css`;
+    expect(linkElement.href).toBe(expectedPath);
+  });
+
+  it('should not throw any errors and return void when given a valid theme mode', () => {
+    expect(() => {
+      themeLoaderService.switchTheme(ThemeMode.Light);
+    }).not.toThrowError();
   });
 });
