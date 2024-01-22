fa0c651e8d70f9351fd169213ca683b365d6fbae
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-01-22 02:25:10 +0100

    Refactor donations service, so it matches the collections service

--- src/app/services/donations.service.ts
+++ src/app/services/donations.service.ts
@@ -1,125 +1,186 @@
 import {inject, Injectable} from '@angular/core';
-import {SupabaseDatabaseService} from "./supabase/supabase-database.service";
-import {BehaviorSubject, map} from "rxjs";
+import {BehaviorSubject, combineLatestWith, from, map, retry, switchMap, throwError} from "rxjs";
 import {Donation} from "../models/donation";
 import {Collection} from "../models/collection";
 import {Cube} from "../models/cube";
 import {Card} from "../models/card";
+import {SupabaseClientService} from "./supabase/supabase-client.service";
+import {RequestState} from "../helpers/request-state.enum";
+import {Tables} from "../models/supabase";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonationsService {
-  private readonly databaseService = inject(SupabaseDatabaseService);
+  private readonly supabase = inject(SupabaseClientService);
 
-  private donations = new BehaviorSubject<Map<number, Donation> | null>(null);
-  donations$ = this.donations.asObservable()
-    .pipe(
-      map(donationMap => {
-        return donationMap
-          ? Array.from(donationMap.values())
-          : null
-      })
+  private requestState = new BehaviorSubject<RequestState>(RequestState.Initial);
+  requestState$ = this.requestState.asObservable();
+
+  private donations = new BehaviorSubject<Map<number, Donation>>(
+    new Map<number, Donation>()
+  );
+  donations$ = this.donations.asObservable().pipe(
+    map(donationMap => Array.from(donationMap.values()))
+  );
+
+  private selectedDonation = new BehaviorSubject<number | null>(null);
+  selectedDonation$ = this.donations$.pipe(
+    combineLatestWith(this.selectedDonation.asObservable()),
+    map(([donations, chosenDonationID]) =>
+      donations.find(donation => donation.id == chosenDonationID)
+    )
+  );
+
+  getDonations() {
+    if (this.requestState.getValue() == RequestState.Success) {
+      return this.donations$;
+    } else {
+      return this.fetchDonations();
+    }
+  }
+
+  fetchDonations() {
+    this.setRequestState(RequestState.InProgress);
+
+    return from(
+      this.supabase.client
+        .from('donations')
+        .select()
+        .returns<Tables<'donations'>[]>()
+    ).pipe(
+      switchMap(result => {
+        if (result.data && !result.error) {
+          const fetchedDonations = result.data.map(
+            value => new Donation(value)
+          );
+          this.donations.next(this.convertArrayToMap(fetchedDonations));
+          this.setRequestState(RequestState.Success);
+          return this.donations$;
+        } else {
+          this.setRequestState(RequestState.Failure);
+          return throwError(() => new Error(result.error.message));
+        }
+      }),
+      retry(2)
     );
-
-  private selectedDonation = new BehaviorSubject<Donation | null>(null);
-  selectedDonation$ = this.selectedDonation.asObservable();
-
-  private requestInProgress = new BehaviorSubject<boolean>(false);
-  requestInProgress$ = this.requestInProgress.asObservable();
-
-  async initializeDonations() {
-    const donationsValue = this.donations.getValue();
-
-    if (!donationsValue || !donationsValue.size) {
-      await this.getDonations();
-    }
   }
 
-  async getDonations() {
-    this.requestInProgress.next(true);
+  createDonation(collection: Collection, cube: Cube) {
+    this.setRequestState(RequestState.InProgress);
+    const filteredCards = collection.cardIDs.filter(id => cube.cardIDs.includes(id));
 
-    const donations = await this.databaseService.fetchDonations();
+    return from(
+      this.supabase.client
+        .from('donations')
+        .insert({ name: `${cube.name} donation`, cube_id: cube.id, card_ids: filteredCards })
+        .select()
+        .returns<Tables<'donations'>[]>()
+    ).pipe(
+      switchMap(result => {
+        if (result.data && !result.error) {
+          const newDonation = new Donation(result.data[0]);
 
-    this.requestInProgress.next(false);
+          this.setDonationInState(newDonation);
+          this.setRequestState(RequestState.Success);
 
-    if (donations) {
-      this.donations.next(donations);
-    }
+          return this.getDonations();
+        } else {
+          this.setRequestState(RequestState.Failure);
+          return throwError(() => new Error(result.error.message));
+        }
+      }),
+      retry(2)
+    );
   }
 
-  clearFetchedDonations() {
-    this.donations.next(null);
+  deleteDonation(donation: Donation) {
+    this.setRequestState(RequestState.InProgress);
+
+    return from(
+      this.supabase.client
+        .from('donations')
+        .delete()
+        .eq('id', donation.id)
+    ).pipe(
+      switchMap(result => {
+        if (!result.error) {
+          this.removeDonationFromState(donation);
+
+          this.setRequestState(RequestState.Success);
+          return this.getDonations();
+        } else {
+          this.setRequestState(RequestState.Failure);
+          return throwError(() => new Error(result.error.message));
+        }
+      }),
+      retry(2)
+    );
   }
 
-  async createDonation(collection: Collection, cube: Cube) {
-    this.requestInProgress.next(true);
-
-    const createdDonation = await this.databaseService.createDonationForCube(collection, cube);
-
-    if (createdDonation) {
-      const currentDonations = this.donations.getValue() ?? new Map<number, Donation>();
-
-      this.donations.next(currentDonations.set(createdDonation.id, createdDonation));
-    }
-
-    this.requestInProgress.next(false);
-  }
-
-  async removeCardsFromDonation(donation: Donation, cards: Card[]) {
-    this.requestInProgress.next(true);
+  removeCardsFromDonation(donation: Donation, cards: Card[]) {
+    this.setRequestState(RequestState.InProgress);
 
     const idsToRemove = cards.map(card => card.id);
     const filteredIDs = donation.cardIDs.filter(id => !idsToRemove.includes(id));
 
-    await this.updateDonationInState(donation, filteredIDs);
+    return from(
+      this.supabase.client
+        .from('donations')
+        .update({ card_ids: filteredIDs })
+        .eq('id', donation.id)
+        .select()
+        .returns<Tables<'donations'>[]>()
+    ).pipe(
+      switchMap(result => {
+        if (result.data && !result.error) {
+          const updatedDonation = new Donation(result.data[0]);
 
-    this.requestInProgress.next(false);
+          this.setDonationInState(updatedDonation);
+          this.setRequestState(RequestState.Success);
+
+          return this.getDonations();
+        } else {
+          this.setRequestState(RequestState.Failure);
+          return throwError(() => new Error(result.error.message));
+        }
+      }),
+      retry(2)
+    );
   }
 
-  async deleteDonation(donation: Donation) {
-    this.requestInProgress.next(true);
-
-    const error = await this.databaseService.deleteDonation(donation);
-
-    if (!error) {
-      const donations = this.donations.getValue() ?? new Map<number, Donation>();
-
-      donations.delete(donation.id);
-
-      this.donations.next(donations);
-    }
-
-    this.requestInProgress.next(false);
-  }
-
-  private async updateDonationInState(donation: Donation, cardIDs: string[]) {
-    const donations = this.donations.getValue() ?? new Map<number, Donation>();
-
-    const error = await this.databaseService.updateDonationCards(donation.id, cardIDs);
-
-    if (!error) {
-      donation.cardIDs = cardIDs;
-      donations.set(donation.id, donation);
-
-      this.donations.next(donations);
-      this.emitSelectedDonationIfMatches(donation);
-    }
-  }
-
-  private emitSelectedDonationIfMatches(donation: Donation) {
-    const selectedDonation = this.selectedDonation.getValue();
-
-    if (selectedDonation && selectedDonation.id === donation.id) {
-      this.selectedDonation.next(donation);
-    }
+  clearFetchedDonations() {
+    this.donations.next(new Map<number, Donation>());
+    this.setRequestState(RequestState.Initial);
   }
 
   selectDonation(donation: Donation) {
-    this.selectedDonation.next(donation);
+    this.selectedDonation.next(donation.id);
   }
 
   clearSelectedDonation() {
     this.selectedDonation.next(null);
+  }
+
+  private convertArrayToMap(array: Donation[]) {
+    return new Map(array.map(donation => [donation.id, donation]));
+  }
+
+  private setDonationInState(donation: Donation) {
+    const donationsState = this.donations.getValue();
+
+    this.donations.next(donationsState.set(donation.id, donation));
+  }
+
+  private removeDonationFromState(donation: Donation) {
+    const donationsState = this.donations.getValue();
+
+    donationsState.delete(donation.id);
+
+    this.donations.next(donationsState);
+  }
+
+  private setRequestState(state: RequestState) {
+    this.requestState.next(state);
   }
 }
